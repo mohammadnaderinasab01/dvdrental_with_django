d8b1ecc319060e07fe2e54d60b1eead89f05caeb
mohammadnaderinasab01 <mohammad_naderinasab@yahoo.com>
2025-03-12 07:03:47 +0330

    change the way send_inventory_rental_overdue_email task sends emails to customers from single threded approach to multithread approach using thread pool.

--- admin_panel/tasks.py
+++ admin_panel/tasks.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 from celery import shared_task
 from django.conf import settings
 import smtplib
@@ -35,6 +36,22 @@ def send_inventory_rental_overdue_email():
             print(f"Failed to send email to {customer.email}: {e}")
 
 
+def send_email_to_customer(customer):
+    subject = "DVDRental - Missing You"
+    message = f"Hi Dear {customer.first_name},\n\nIt's now over 60 days that we do not see you in our app.\nThis is Just a message for you to see that \"we miss you :)\". Just come back soon... :)"
+    try:
+        connection = smtplib.SMTP(settings.MAIL_SERVER_HOST)
+        connection.starttls()
+        connection.login(user=settings.NOTIFIER_EMAIL_ADDRESS, password=settings.NOTIFIER_EMAIL_PASSWORD)
+        connection.sendmail(
+            from_addr=settings.NOTIFIER_EMAIL_ADDRESS,
+            to_addrs=customer.email,
+            msg=f"Subject:{subject}\n\n{message}")
+        connection.close()
+    except smtplib.SMTPAuthenticationError as e:
+        print(f"Failed to send email to {customer.email}: {e}")
+
+
 @shared_task
 def inactive_customers_activity_email_notifier():
     try:
@@ -46,17 +63,25 @@ def inactive_customers_activity_email_notifier():
     inactive_customers = Customer.objects.filter(
         rental__rental_date__lt=last_60_days,
         rental__return_date__lt=last_60_days)
-    for cst in inactive_customers:
-        subject = "DVDRental - Missing You"
-        message = f"Hi Dear {cst.first_name},\n\nIt's now over 60 days that we do not see you in our app.\nThis is Just a message for you to see that \"we miss you :)\". Just come back soon... :)"
-        try:
-            connection = smtplib.SMTP(settings.MAIL_SERVER_HOST)
-            connection.starttls()
-            connection.login(user=settings.NOTIFIER_EMAIL_ADDRESS, password=settings.NOTIFIER_EMAIL_PASSWORD)
-            connection.sendmail(
-                from_addr=settings.NOTIFIER_EMAIL_ADDRESS,
-                to_addrs=cst.email,
-                msg=f"Subject:{subject}\n\n{message}")
-            connection.close()
-        except smtplib.SMTPAuthenticationError as e:
-            print(f"Failed to send email to {cst.email}: {e}")
+    if not inactive_customers.exists():
+        print("No inactive customers found.")
+        return
+    try:
+        with ThreadPoolExecutor(max_workers=10) as executor:  # Limit to 10 threads
+            executor.map(send_email_to_customer, inactive_customers)
+    except:
+        print('an error occurred in thread pool side.')
+    # for cst in inactive_customers:
+    #     subject = "DVDRental - Missing You"
+    #     message = f"Hi Dear {cst.first_name},\n\nIt's now over 60 days that we do not see you in our app.\nThis is Just a message for you to see that \"we miss you :)\". Just come back soon... :)"
+    #     try:
+    #         connection = smtplib.SMTP(settings.MAIL_SERVER_HOST)
+    #         connection.starttls()
+    #         connection.login(user=settings.NOTIFIER_EMAIL_ADDRESS, password=settings.NOTIFIER_EMAIL_PASSWORD)
+    #         connection.sendmail(
+    #             from_addr=settings.NOTIFIER_EMAIL_ADDRESS,
+    #             to_addrs=cst.email,
+    #             msg=f"Subject:{subject}\n\n{message}")
+    #         connection.close()
+    #     except smtplib.SMTPAuthenticationError as e:
+    #         print(f"Failed to send email to {cst.email}: {e}")
