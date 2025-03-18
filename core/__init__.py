bdb3536388f06a42079a1499c7c4029bf25dde54
mohammadnaderinasab01 <mohammad_naderinasab@yahoo.com>
2025-03-18 06:10:41 +0330

    add pymongo wrapper connect method in __init__.py of core app to run after the project starts.

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,9 @@
 # dvdrental_project/__init__.py
 from .celery import app as celery_app
+from pymongo_wrapper.connection import connect
 
 __all__ = ('celery_app',)
+
+
+# Initialize the connection once at startup
+mongo_dvdrental_db = connect("dvdrental_django_project_test", "mongodb://127.0.0.1:27017")
