b7f5ffc54de1704df2ea8a571aeecbc33eda1fd9
mohammadnaderinasab01 <mohammad_naderinasab@yahoo.com>
2025-05-10 04:18:11 +0330

    add sort_by to queries API

--- database_profiler/serializers.py
+++ database_profiler/serializers.py
@@ -1,5 +1,15 @@
 from rest_framework import serializers
 
 
+class QueriesRequestSerializer(serializers.Serializer):
+    sort_by = serializers.ChoiceField(
+        choices=["execution_duration", "execution_time", "row_affected"],
+        required=False,
+        error_messages={
+            "invalid_choice": "Invalid choice. Allowed values are: execution_duration, execution_time, row_affected."
+        }
+    )
+
+
 class QueriesSerializer(serializers.Serializer):
     queries = serializers.ListField()
--- database_profiler/views.py
+++ database_profiler/views.py
@@ -1,14 +1,33 @@
 from rest_framework import views
 from utils.responses import CustomResponse
 from pymongo_wrapper.model import Query
-from .serializers import QueriesSerializer
+from .serializers import QueriesSerializer, QueriesRequestSerializer
+from drf_spectacular.utils import extend_schema
 
 
 class QueriesView(views.APIView):
+    @extend_schema(parameters=[QueriesRequestSerializer])
     def get(self, request):
-        serializer = QueriesSerializer(Query.find(), many=True)
-        # print('serializer: ', serializer.data)
-        print('result: ', (list(Query.find())))
+        request_serializer = QueriesRequestSerializer(data=request.query_params)
+        if not request_serializer.is_valid():
+            return CustomResponse.bad_request(request_serializer.errors)
+
+        serializer = QueriesSerializer(
+            Query.aggregate([
+                {
+                    "$set": {
+                        "queries": {
+                            "$sortArray": {
+                                "input": "$queries",
+                                "sortBy": {request_serializer.validated_data.get('sort_by'): -1}
+                            }
+                        }
+                    }
+                },
+                {
+                    "$sort": {f"queries.0.{request_serializer.validated_data.get('sort_by')}": -1}
+                }
+            ]), many=True)
         try:
             return CustomResponse.successful_200(serializer.data)
         except Exception as e:
